852bf659303690c38e45057b3ecebe8a8aa59142
Maria Guadalupe Montiel <90074588+MGMontiel@users.noreply.github.com>
2023-09-25 19:42:23 -0500

    Update Predictiosn_figure_example.R

--- Predictiosn_figure_example.R
+++ Predictiosn_figure_example.R
@@ -7,21 +7,10 @@ library(hrbrthemes)
 
 ## Figure 03
 
-setwd("~/LSU Maria Montiel/Projects/project_mp/project_mp_01paper/project_mp_epistasis/Cross validation/results/")
+setwd("")
 my_palette <- c("#4682B4","#FFA500","#EE3A8C", "#2F4F4F")
 res <- read.csv("corr_all.csv")
 
-# barplot of every trial by A and AI
-ggplot(res, aes(x = trial , y = Accuracy, fill = Model)) +
-  geom_bar(stat = "summary", position = "dodge", color = "black", fun ="mean") +
-  scale_fill_manual(values = my_palette) +
-  facet_wrap(~trait, scales = "free_x") +
-  theme_bw(base_size = 14) +
-  ggtitle("Cross Validation Predictive Ability of the MP Materials comparing A versus AI effects") +
-  scale_y_continuous(limits = c(0, 0.8), breaks = seq(0, 1, by = 0.2))+
-  theme(legend.position = "bottom", plot.title = element_text(hjust = 0.5, size = 16, lineheight = 1.2)) +
-  labs(y = "Predictive ability", x = "Trials") +
-  ggtitle("Cross Validation Predictive Ability of the MP Materials comparing A versus AI effects")
 
 # boxplot for every trial
 
